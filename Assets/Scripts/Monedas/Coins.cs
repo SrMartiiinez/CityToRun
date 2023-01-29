3afe0522bc5026d7a17f2fbb2a8fb90d4de8ba0b
SrMartiiinez <gmartinezc@esimurcia.com>
2023-01-29 01:26:37 +0100

    Correcion monedas
    Habia un problema en las monedas, cuando pasas por un tramo de monedas, en el proximo spaw tileable dejaba la funcion de recoger monedas y lo atravasabas, entonces coregi el codigo y ahora funcionan.

--- Assets/Scripts/Monedas/Coins.cs
+++ Assets/Scripts/Monedas/Coins.cs
@@ -13,6 +13,11 @@ public class Coins : MonoBehaviour
     public EventSO pickup;
     private bool collectedCoin;
 
+    void OnEnable()
+    {
+        collectedCoin = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -21,14 +26,13 @@ public class Coins : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player" && !collectedCoin)
+        if (other.tag == "Player" && !collectedCoin)
         {
             collectedCoin = true;
             Character.numberOfCoins += 1;
-            //Debug.Log("Coins:" + Character.numberOfCoins);
+
             pickup.Ocurred(gameObject);
-            //AnimParticleCoin.instance.TriggerCoinAnimation(); // Activa animacion de moneda recogida
-            //Destroy(transform.parent.gameObject);
+
             transform.parent.gameObject.SetActive(false);
         }
     }
